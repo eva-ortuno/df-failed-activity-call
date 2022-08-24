357cdc56b828db62ca26b2c7f448ac9489890770
Eva Ortuno <ortuno.eva.pro@gmail.com>
2022-08-24 11:32:36 +0200

    Initial commit

---
+++ DO_main/index.js
@@ -0,0 +1,14 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    const input = context.df.getInput();
+    console.log(input);
+
+    // Replace "Hello" with the name of your Durable Activity Function.
+    outputs.push(yield context.df.callActivity("DA_fail", "Tokyo"));
+
+    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+    return outputs;
+});
